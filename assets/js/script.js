8f7755a2d42da768ba889c3f26ebccb5aa8dafab
DennisMugarte <a.erwin.castellanos@utponiente.edu.mx>
2023-02-12 10:43:59 -0600

    cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,61 @@ let login_register = document.querySelector(".login-register");
 let caja_trasera_login = document.querySelector(".caja__trasera-login");
 let caja_trasera_register = document.querySelector(".caja__trasera-register");
 
+
 function register() {
-    formulario_register.style.display = "block";
-    login_register.style.left = "410px";
-    formulario_login.style.display = "none";
-    caja_trasera_register.style.opacity = "0";
-    caja_trasera_login.style.opacity = "1";
+    if (window.innerWidth > 850) {
+        formulario_register.style.display = "block";
+        login_register.style.left = "410px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.opacity = "0";
+        caja_trasera_login.style.opacity = "1";
+    } else {
+        formulario_register.style.display = "block";
+        login_register.style.left = "0px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.display = "none";
+        caja_trasera_login.style.display = "block";
+        caja_trasera_login.style.opacity = "1";
+    }
 }
+
+
+function iniciarSesion() {
+    if (window.innerWidth > 850) {
+        formulario_register.style.display = "none";
+        login_register.style.left = "10px";
+        formulario_login.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.opacity = "0";
+    } else {
+        formulario_register.style.display = "none";
+        login_register.style.left = "0px";
+        formulario_login.style.display = "block";
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.display = "none";
+    }
+}
+
+function anchoPage() {
+
+    if (window.innerWidth > 850) {
+        caja_trasera_login.style.display = "block";
+        caja_trasera_register.style.display = "block";
+
+    } else {
+        caja_trasera_register.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.display = "none";
+        formulario_login.style.display = "block";
+        contenedor_login_register.style.left = "0px";
+        formulario_register.style.display = "none";
+    }
+}
+
+anchoPage();
+
 
 ///////////////////////////  FUNCON DE LOS BOTONES ////////////////////////////////////////
 document.getElementById("btn__registrarse").addEventListener("click", register);
+document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
+window.addEventListener("resize", anchoPage);
