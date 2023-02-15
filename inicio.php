0c77469cc54ed138c322dba41fcac4ebe07dee27
DennisMugarte <125104128+ErwinCastellanos@users.noreply.github.com>
2023-02-14 18:49:42 -0600

    Cambios

---
+++ inicio.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Pagina web basica</title>
+
+</head>
+<body>
+
+    <div class="head">
+
+        <div class="logo">
+            <a href="#">UTPoniete</a>
+        </div>
+
+        <nav class="navbar">
+            <a href="#">Inicio</a>
+            <a href="#">Nosotros</a>
+         
+            <a href="#">Registraté</a>
+            <a href="#">Precio</a>
+        </nav>
+
+    </div>
+
+    <header class="content header">
+        <br>
+        <br>
+        <br>
+        <h2 class="title">5 lenguajes de programación más usados en 2023</h2>
+        <p>
+            A continuación, repasamos las características más importantes de cada uno de estos lenguajes, que son los mejores lenguajes de programación en 2023.
+        </p>
+
+        <div class="btn-home">
+            <a href="#" class="btn">Saber mas</a>
+            <a href="#" class="btn">Saber mas</a>
+        </div>
+    </header>
+
+    <section class="content sau">
+        <br>
+        <br>
+        <br>
+        <h2 class="title">Python</h2>
+        <p>Python es uno de los lenguajes de programación más usados actualmente y su uso sigue creciendo. Posee unas características muy potentes: es de código abierto, tiene una sintaxis sencilla y es fácil de entender, por lo que ahorra tiempo y recursos.
+            <br>
+            Python es un lenguaje versátil que puede tener múltiples aplicaciones. Una de ellas, es la Inteligencia Artificial, gracias a bibliotecas como Keras o TensorFlow.</p>
+        
+        <div class="box-container">
+
+            <div class="box">
+                <i class="fab fa-angular"></i>
+                <h3>ANGULAR</h3>
+                <p>Angular es un framework para aplicaciones web desarrollado en TypeScript, de código abierto.</p>
+            </div>
+            <div class="box">
+                <i class="fab fa-apple"></i>
+                <h3>APPLE</h3>
+                <p>Apple, Inc. es una empresa estadounidense que diseña y produce equipos electrónicos, software y servicios en línea.</p>
+            </div>
+            <div class="box">
+                <i class="fab fa-android"></i>
+                <h3>ANDROID</h3>
+                <p>Android es un sistema operativo móvil basado en el núcleo Linux y otros software de código abierto.</p>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section class="content about">
+        <br>
+        <br>
+        <br>
+        <h2 class="title">Nosotros</h2>
+        <p>El objetivo que nos llevó a existir fue el de llevar a todos los rincones de la Galaxia desarrolladores de calidad. Porque no basta con saber programar, hay que hacerlo muy bien.
+        </p>
+
+        <a href="#" class="btn">Saber mas</a>
+
+    </section>
+
+    <section class="content price">
+
+        <article class="contain">
+            <h2 class="title">Inicia Sesión</h2>
+           
+
+            <a href="index.php" class="btn">Registraté</a>
+
+        </article>
+
+    </section>
+
+   
+    
+</body>
+</html>
